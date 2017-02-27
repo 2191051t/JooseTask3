fdebcd92e40675e653b8a32069d9c18bb91e4199
Greg Tucker <2191051t@student.gla.ac.uk>
2017-02-27 19:23:53 +0000

    started InitBoardBehaviour

---
+++ src/main/java/gla/joose/birdsim/boards/FlockInitBoard.java
@@ -0,0 +1,10 @@
+package gla.joose.birdsim.boards;
+
+import javax.swing.JFrame;
+
+public class FlockInitBoard implements InitBoardBehaviour {
+
+	public void initBoard(JFrame frame, Board board) {
+		
+	}
+}
---
+++ src/main/java/gla/joose/birdsim/boards/InitBoardBehaviour.java
@@ -0,0 +1,7 @@
+package gla.joose.birdsim.boards;
+
+import javax.swing.JFrame;
+
+public interface InitBoardBehaviour {
+	public void initBoard(final JFrame frame, Board board);
+}
